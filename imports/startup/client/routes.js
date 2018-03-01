c6ffb9991b801687707e5597d43b304589680ac0
AMeLaMela <alv67.web@gmail.com>
2018-03-01 23:33:36 +0100

    Creazione del "header.html" e aggiunta degli stili presi da "Getting Started with Meteor" by Packt publising

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -3,6 +3,7 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 // Import needed templates
 import '../../ui/layouts/body/body.js';
+import '../../ui/layouts/header/header.js';
 import '../../ui/pages/home/home.js';
 import '../../ui/pages/not-found/not-found.js';
 
